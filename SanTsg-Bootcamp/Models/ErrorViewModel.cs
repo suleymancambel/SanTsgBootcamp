5c66c3a3be7b8cdb37915ede39e5149750bbfafb
süleyman <cambelsuleyman@gmail.com>
2022-06-02 12:05:58 +0300

    create project

---
+++ SanTsg-Bootcamp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SanTsg_Bootcamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
